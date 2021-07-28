6522d5d89227745c3563d741a32704affa60b27c
Justin <jstn.anthony1@gmail.com>
2021-07-28 19:05:12 -0400

    ecard & template models

---
+++ MHC_Hospital_Redesign/Migrations/202107282301012_template-ecards.cs
@@ -0,0 +1,46 @@
+﻿namespace MHC_Hospital_Redesign.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class templateecards : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Ecards",
+                c => new
+                    {
+                        EcardId = c.Int(nullable: false, identity: true),
+                        SenderName = c.String(),
+                        PatientName = c.String(),
+                        Message = c.String(),
+                        TemplateId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.EcardId)
+                .ForeignKey("dbo.Templates", t => t.TemplateId, cascadeDelete: true)
+                .Index(t => t.TemplateId);
+            
+            CreateTable(
+                "dbo.Templates",
+                c => new
+                    {
+                        TemplateId = c.Int(nullable: false, identity: true),
+                        TemplateName = c.String(),
+                        TemplateHasPic = c.Boolean(nullable: false),
+                        TemplatePicExtension = c.String(),
+                        TemplateStyle = c.String(),
+                    })
+                .PrimaryKey(t => t.TemplateId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Ecards", "TemplateId", "dbo.Templates");
+            DropIndex("dbo.Ecards", new[] { "TemplateId" });
+            DropTable("dbo.Templates");
+            DropTable("dbo.Ecards");
+        }
+    }
+}
--- MHC_Hospital_Redesign/Models/Ecard.cs
+++ MHC_Hospital_Redesign/Models/Ecard.cs
@@ -6,6 +6,8 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
+
 namespace MHC_Hospital_Redesign.Models
 {
     public class Ecard
