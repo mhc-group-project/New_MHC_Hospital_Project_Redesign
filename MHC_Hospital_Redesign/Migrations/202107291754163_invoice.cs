5e8cc39cc716cc6cd3e1dbd5bedeae6c3f6c4043
Natasha Chambers <natashaagathachambers@gmail.com>
2021-07-29 15:07:08 -0400

    added invoice and payment models

---
+++ MHC_Hospital_Redesign/Migrations/202107291754163_invoice.cs
@@ -0,0 +1,29 @@
+﻿namespace MHC_Hospital_Redesign.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class invoice : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Invoices",
+                c => new
+                    {
+                        InvoiceID = c.Int(nullable: false, identity: true),
+                        InvoiceNumber = c.Int(nullable: false),
+                        InvoiceDate = c.DateTime(nullable: false),
+                        Amount = c.Int(nullable: false),
+                        Currency = c.String(),
+                    })
+                .PrimaryKey(t => t.InvoiceID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Invoices");
+        }
+    }
+}
---
+++ MHC_Hospital_Redesign/Models/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MHC_Hospital_Redesign.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int PaymentID { get; set; }
+        public string NameOnCard { get; set; }
+        public string CardHash { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+
+        [ForeignKey("Invoice")]
+        public int InvoiceID { get; set; }
+        public virtual Invoice Invoice { get; set; }
+    }
+}
