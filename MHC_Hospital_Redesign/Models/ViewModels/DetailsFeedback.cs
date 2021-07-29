841a1128f9981e2bd70e9625f8490c28ea12b7b9
Yifat-t <78460287+Yifat-t@users.noreply.github.com>
2021-07-29 13:01:06 -0400

    Update DetailsFeedback.cs

--- MHC_Hospital_Redesign/Models/ViewModels/DetailsFeedback.cs
+++ MHC_Hospital_Redesign/Models/ViewModels/DetailsFeedback.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HospitalProject.Models.ViewModels
+namespace MHC_Hospital_Redesign.Models.ViewModels
 {
     public class DetailsFeedback
     {
 
         public FeedbackDto SelectedFeedback { get; set; }
-        public DepartmentDto SelectedDepartment { get; set; }
+       
         public FeedbackCategoryDto RelatedFeedbackCategory { get; set; }
 
     }
