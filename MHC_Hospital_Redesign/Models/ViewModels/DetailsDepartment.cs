b713c86ebc159728c26c0c2da8c91c0426b6efdd
Yifat-t <78460287+Yifat-t@users.noreply.github.com>
2021-07-29 13:00:28 -0400

    Update DetailsDepartment.cs

--- MHC_Hospital_Redesign/Models/ViewModels/DetailsDepartment.cs
+++ MHC_Hospital_Redesign/Models/ViewModels/DetailsDepartment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HospitalProject.Models.ViewModels
+namespace MHC_Hospital_Redesign.Models.ViewModels
 {
     public class DetailsDepartment
     {
