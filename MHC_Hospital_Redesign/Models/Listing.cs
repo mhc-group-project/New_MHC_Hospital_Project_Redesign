3626db0459db277c8f8749c33f54cef9941783ad
ruth frogoso <ruthfrogoso@gmail.com>
2021-07-30 09:57:44 -0400

    added foreign key

--- MHC_Hospital_Redesign/Models/Listing.cs
+++ MHC_Hospital_Redesign/Models/Listing.cs
@@ -16,7 +16,10 @@ namespace MHC_Hospital_Redesign.Models
         public string ListDescription { get; set; }
         public string ListRequirements { get; set; }
         public string ListLocation { get; set; }
-        public int DepartmentID { get; set; } //foreign key
+        // 1-M relationship with Department
+        // a listing can belong to a department, a department can have many listings
+        [ForeignKey("Department")]
+        public int DepartmentID { get; set; }
 
         // M-M relationship with Volunteer Users
         public ICollection<ApplicationUser> ApplicationUsers { get; set; }
