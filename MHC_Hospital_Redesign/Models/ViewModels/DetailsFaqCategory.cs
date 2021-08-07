8ce789d648bd9e743e468a9f8fa1ce1bce9db24f
Neil Moras <neilmoras@gmail.com>
2021-08-07 18:49:06 -0400

    added basic css-faq

--- MHC_Hospital_Redesign/Models/ViewModels/DetailsFaqCategory.cs
+++ MHC_Hospital_Redesign/Models/ViewModels/DetailsFaqCategory.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MHC_Hospital_Redesign.Models.ViewModels
 {
     public class DetailsFaqCategory
     {
 
-       
-       
-            public FaqCategoryDto SelectedFaqCategory { get; set; }
+
+        [AllowHtml]
+        public FaqCategoryDto SelectedFaqCategory { get; set; }
+        [AllowHtml]
             public IEnumerable<FaqDto> LinkedFaqs { get; set; }
        
     }
